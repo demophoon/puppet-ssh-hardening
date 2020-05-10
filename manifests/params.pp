b3aab620d6b6a2680ba20475a955b22f601aa4e2
Britt Gresham <britt@brittg.com>
2020-05-10 13:27:30 -0700

    Add fallback MACs

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,6 +11,8 @@ class ssh_hardening::params {
         $macs = 'hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160'
     } elsif versioncmp($ssh_server_version_major, '7.6') <= 0 {
         $macs = 'hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha2-256,hmac-sha2-512'
+    } else {
+        $macs = 'hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha2-256,hmac-sha2-512'
     }
   }
 
